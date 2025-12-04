76be512459947c3b8fddd8ade83749b429c20672
EDUARDO ROBERTO RODRIGUES <eduardo_r_rodrigues@estudante.sc.senai.br>
2025-12-04 16:51:06 -0300

    sistema-bancada

---
+++ fronend/bancada/bancada.js
@@ -0,0 +1,5 @@
+function login(e){
+    e.preventDefault()
+    console.log("redirecionando")
+    window.location.href = "../index.html"
+}
--- fronend/index.js
+++ fronend/index.js
@@ -1,26 +1,5 @@
-function login() {
-    let usuario = document.getElementById("usuario").value;
-    let senha = document.getElementById("senha").value;
-    let msg = document.getElementById("mensagem");
-
-    // Usuário e senha de exemplo
-    let usuarioCorreto = "lula";
-    let senhaCorreta = "13";
-
-    if (usuario === usuarioCorreto && senha === senhaCorreta) {
-        msg.style.color = "green";
-        msg.textContent = "Login realizado com sucesso!";
-
-        setTimeout(() => {
-            // ALERTA (opcional)
-            alert("Bem-vindo!");
-
-            // 👉 AQUI VEM O REDIRECIONAMENTO
-            window.location.href = "https://www.google.com"; 
-        }, 500);
-
-    } else {
-        msg.style.color = "red";
-        msg.textContent = "Usuário ou senha incorretos!";
-    }
+function login(e){
+    e.preventDefault()
+    console.log("redirecionando")
+    window.location.href = "bancada/bancada.html"
 }
